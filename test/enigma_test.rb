ed26be2c89289991569486a45f3db52268766a52
Maria Ronauli <mronauli@gmail.com>
2020-01-15 02:19:19 -0700

    add test for encrypt and decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,11 +14,32 @@ class KeyTest < Minitest::Test
   def test_enigma_has_an_alphabet_with_27_characters
     enigma = Enigma.new
     expected =
-    ["a", "b", "c", "d", "e", "f", "g", "h", 
+    ["a", "b", "c", "d", "e", "f", "g", "h",
       "i", "j", "k", "l", "m", "n", "o", "p",
       "q", "r", "s", "t", "u", "v", "w", "x",
       "y", "z", " "]
+
     assert_equal expected, enigma.alphabet
     assert_equal 27, enigma.alphabet.length
+  end
+
+  def test_encrypt
+    enigma = Enigma.new
+    my_key = Key.new
+    offset = Offset.new("010419")
+    shift = Shift.new(["12", "23", "34", "45"], ["0", "4", "0", "0"])
+    assert_instance_of Key, my_key
+    assert_instance_of Offset, offset
+    assert_instance_of Shift, shift
+  end
+
+  def test_decrypt
+    enigma = Enigma.new
+    my_key = Key.new
+    offset = Offset.new("010419")
+    shift = Shift.new(["12", "23", "34", "45"], ["0", "4", "0", "0"])
+    assert_instance_of Key, my_key
+    assert_instance_of Offset, offset
+    assert_instance_of Shift, shift
   end
 end
