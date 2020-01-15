7220bae4bcf3651a31e49e6d53d386295956a395
Maria Ronauli <mronauli@gmail.com>
2020-01-15 08:28:05 -0700

    clean up indentatioin

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,10 +14,15 @@ class KeyTest < Minitest::Test
   def test_enigma_has_an_alphabet_with_27_characters
     enigma = Enigma.new
     expected =
-    ["a", "b", "c", "d", "e", "f", "g", "h",
-      "i", "j", "k", "l", "m", "n", "o", "p",
-      "q", "r", "s", "t", "u", "v", "w", "x",
-      "y", "z", " "]
+      [
+        "a", "b", "c", "d",
+        "e", "f", "g", "h",
+        "i", "j", "k", "l",
+        "m", "n", "o", "p",
+        "q", "r", "s", "t",
+        "u", "v", "w", "x",
+        "y", "z", " "
+      ]
 
     assert_equal expected, enigma.alphabet
     assert_equal 27, enigma.alphabet.length
