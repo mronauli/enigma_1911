084b3dd336643d9995ca1e7cbb2707e443fa617e
Maria Ronauli <mronauli@gmail.com>
2020-01-14 19:44:42 -0700

    refactor for simplecov

--- lib/offset.rb
+++ lib/offset.rb
@@ -2,14 +2,11 @@ require 'date'
 
 class Offset
   attr_reader :date
-  def initialize(date = nil)
+  def initialize(date = Time.now.strftime("%d%m%y").to_i)
     @date = date
   end
 
   def square
-    if @date == nil
-      @date = Time.now.strftime("%d%m%y").to_i
-    end
     (@date.to_i * @date.to_i)
   end
 
