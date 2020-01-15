414e694b623f593394cd687c1bdd3544a911a9f9
Maria Ronauli <mronauli@gmail.com>
2020-01-15 03:41:17 -0700

    add decrypt method which does almost the exact same thing as encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,16 +12,17 @@ class Enigma
     counter = 4
 
     encrypted = ""
+
     characters = message.downcase.chars
     characters.each do |char|
       index = @alphabet.find_index(char)
       which_offset = counter % 4
-      if index != nil
-        new_index = (index + shift.offsets[which_offset].to_i + shift.keys[which_offset].to_i) % @alphabet.length
-        encrypted += @alphabet[new_index]
-      else
+        if index != nil
+          new_index = (index + shift.offsets[which_offset].to_i + shift.keys[which_offset].to_i) % @alphabet.length
+          encrypted += @alphabet[new_index]
+        else
         encrypted += char
-      end
+    end
       counter += 1
     end
     {:encryption => encrypted, :key => my_key.numbers.join(""), :date => offset.date}
@@ -30,6 +31,23 @@ class Enigma
   def decrypt(message, key, date = nil)
     my_key = Key.new(key)
     offset = Offset.new(date)
-    shift = Shift.new(my_key.combine, offset.last_f)
+    shift = Shift.new(my_key.combine, offset.last_four)
+
+    counter = 4
+
+    decrypted = ""
+
+    message.chars.each do |char|
+      index = @alphabet.find_index(char)
+      which_offset = counter % 4
+        if index != nil
+          new_index = (index - shift.offsets[which_offset].to_i - shift.keys[which_offset].to_i) % @alphabet.length
+          decrypted += @alphabet[new_index]
+        else
+          decrypted += char
+      end
+      counter += 1
+    end
+    {:decryption => decrypted, :key => my_key.numbers.join(""), :date => offset.date}
   end
 end
