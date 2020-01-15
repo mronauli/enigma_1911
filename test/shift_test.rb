2b97538b8d6beb997797c54e20a87c57ab6cc60d
Maria Ronauli <mronauli@gmail.com>
2020-01-15 01:49:19 -0700

    add tests for shift attributes keys and offsets

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,7 +5,13 @@ require './lib/shift'
 
 class ShiftTest < Minitest::Test
   def test_shift_exists
-    shift = Shift.new(key = ["12", "23", "34", "45"], offset = ["0", "4", "0", "0"])
+    shift = Shift.new(["12", "23", "34", "45"], ["0", "4", "0", "0"])
     assert_instance_of Shift, shift
+  end
+
+  def test_shift_has_keys_and_offsets
+    shift = Shift.new(["12", "23", "34", "45"], ["0", "4", "0", "0"])
+    assert_equal ["12", "23", "34", "45"], shift.keys
+    assert_equal ["0", "4", "0", "0"], shift.offsets
   end
 end
